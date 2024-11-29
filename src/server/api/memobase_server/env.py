dc83b5f54ed93c19cb61056d855403da375d93ba
Gus <jianbaiye@outlook.com>
2024-11-29 13:22:23 +0800

    chore: update default profile size

--- src/server/api/memobase_server/env.py
+++ src/server/api/memobase_server/env.py
@@ -20,7 +20,7 @@ load_dotenv()
 class Config:
     buffer_flush_interval: int = 60 * 60  # 1 hour
     max_chat_blob_buffer_token_size: int = 1024
-    max_pre_profile_token_size: int = 256
+    max_pre_profile_token_size: int = 512
 
     # LLM
     language: str = "en"
