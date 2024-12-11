6a1691fa5030bf4061a77bbf54dc6a468464e9c1
Gus <jianbaiye@outlook.com>
2024-12-11 19:47:45 +0800

    chore: remove sql generate

--- src/server/script/sync-types.sh
+++ src/server/script/sync-types.sh
@@ -3,8 +3,8 @@ set -e
 python ./api/build_init_sql.py > ./db/init.sql
 
 # Get the value of __version__ from memobase_server.__init__
-version=$(grep -oE '__version__ *= *"[^"]+"' ./api/memobase_server/__init__.py | awk -F'"' '{print $2}')
-echo "Version: $version"
+# version=$(grep -oE '__version__ *= *"[^"]+"' ./api/memobase_server/__init__.py | awk -F'"' '{print $2}')
+# echo "Version: $version"
 # echo the current time to a file
 echo "# Synced from backend ${version}" > ../client/memobase/core/blob.py
 cat ./api/memobase_server/models/blob.py >> ../client/memobase/core/blob.py
