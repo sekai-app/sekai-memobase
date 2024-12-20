e0fc1e814926ba61abe7add56f2b7b49ab83bfc9
Gus <jianbaiye@outlook.com>
2024-12-20 17:06:05 +0800

    fix: add ttl for redis key

--- src/server/api/memobase_server/telemetry/capture_key.py
+++ src/server/api/memobase_server/telemetry/capture_key.py
@@ -10,10 +10,11 @@ def head_key():
     return f"memobase_telemetry::{PROJECT_ID}"
 
 
-async def capture_int_key(name, value: int = 1):
+async def capture_int_key(name, value: int = 1, expire_days: int = 14):
     key = f"{head_key()}::{name}::{date_key()}"
     async with get_redis_client() as r_c:
         await r_c.incrby(key, value)
+        await r_c.expire(key, expire_days * 24 * 60 * 60)
 
 
 if __name__ == "__main__":
