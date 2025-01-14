0a07b277be6bfe6540bcfed564a794b055b2c281
Gus <jianbaiye@outlook.com>
2025-01-14 13:40:00 +0800

    bump client to 0.0.4

--- src/server/api/memobase_server/__init__.py
+++ src/server/api/memobase_server/__init__.py
@@ -3,7 +3,3 @@ __version__ = "0.0.3.dev0"
 __author__ = "memobase.io"
 __url__ = "https://github.com/memodb-io/memobase"
 __license__ = "Apache-2.0"
-
-
-# TODO add event capture for interactions (using local redis)
-# TODO add token cost capture (using local redis)
