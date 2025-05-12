f692f45fce6f44a6f054f154e91c196b2f149e1c
Gus <jianbaiye@outlook.com>
2025-05-12 13:04:56 +0800

    fix: summary profile prompt

--- src/server/api/memobase_server/prompts/summary_profile.py
+++ src/server/api/memobase_server/prompts/summary_profile.py
@@ -8,8 +8,10 @@ SUMMARY_PROMPT = """You are given a user profile with some information about the
 ## Requirement
 Summary the given context in concise form, not more than 3 sentences.
 Remove the redundant information and keep the most important information.
+Look for the dates on infos, and always Keep the latest infos in the profile
 总结给定的上下文，简洁形式，不超过3句话。
 去除重复相似的信息，保留最重要的信息。
+留意日期，总是保留最新的信息。
 
 The result should use the same language as the input.
 结果应该使用与输入相同的语言。
