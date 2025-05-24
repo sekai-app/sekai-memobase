0316e80b6b388bd394c471b08e9e8426dd1bf208
Gus <jianbaiye@outlook.com>
2025-05-24 11:25:25 +0800

    fix: remove <memory prompt>

--- src/server/api/memobase_server/prompts/chat_context_pack.py
+++ src/server/api/memobase_server/prompts/chat_context_pack.py
@@ -4,26 +4,30 @@ ContextPromptFunc = Callable[[str, str], str]
 
 
 def en_context_prompt(profile_section: str, event_section: str) -> str:
-    return f"""<memory>
-# Below is the user profile:
+    return f"""---
+# Memory
+## User Background:
 {profile_section}
 
-# Below is the latest events of the user:
+## Latest Events:
 {event_section}
-</memory>
+
 Unless the user has relevant queries, do not actively mention those memories in the conversation.
+---
 """
 
 
 def zh_context_prompt(profile_section: str, event_section: str) -> str:
-    return f"""<memory>
-# 以下是用户的用户画像：
+    return f"""---
+# 记忆
+## 用户背景：
 {profile_section}
 
-# 以下是用户的最近事件：
+## 部分过去事件：
 {event_section}
-</memory>
-除非用户有相关的需求，否则不要主动在对话中提到这些memory.
+
+除非用户有相关的需求，否则不要主动在对话中提到这些记忆.
+---
 """
 
 
